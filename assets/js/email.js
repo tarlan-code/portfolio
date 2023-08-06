9c49de96ab52873a5f08c7874e535aa372eb1864
Tarlan <tu769djc3@code.edu.az>
2023-08-06 14:36:58 +0400

    fixed email send

--- assets/js/email.js
+++ assets/js/email.js
@@ -7,10 +7,20 @@
 window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
+
+
+        // Verify reCAPTCHA response
+        const captchaResponse = grecaptcha.getResponse();
+        if (!captchaResponse) {
+            alert('Please complete the reCAPTCHA.');
+            return;
+        }
+
+
         // generate a five digit number for the contact_number variable
         this.contact_number.value = Math.random() * 100000 | 0;
         // these IDs from the previous steps
-        emailjs.sendForm('service_68mpgj4', 'template_egb4yf9', 'g-recaptcha', this)
+        emailjs.sendForm('service_68mpgj4', 'template_egb4yf9', this)
             .then(function() {
                 console.log('SUCCESS!');
             }, function(error) {
